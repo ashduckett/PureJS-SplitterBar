696cf4fa101a92eb008999085fd6d7166fbb397b
Ash Duckett <ash.duckett@outlook.com>
2018-10-09 13:56:54 +0100

    Removal of redundant mouseIsDown check

--- main.js
+++ main.js
@@ -11,15 +11,11 @@ const SplitterBar = function(container) {
     
     container.appendChild(splitter);
     
-
-
-
-
     splitter.style.width = '10px';
     splitter.style.left = splitter.parentElement.offsetWidth / 2 - (splitter.offsetWidth / 2) + 'px';
 
-    leftSide.style.background = 'red';
-    rightSide.style.background = 'blue';
+    // leftSide.style.background = 'red';
+    // rightSide.style.background = 'blue';
     splitter.style.background = 'black';
 
 
@@ -80,29 +76,21 @@ const SplitterBar = function(container) {
         let containerWidth = container.getBoundingClientRect().width;
         let hoveringOnDocument = evt.target.nodeName == 'HTML' || evt.target.nodeName == 'BODY';
         let docX = evt.offsetX - container.getBoundingClientRect().x - startX;
-
-        if (hoveringOnDocument) {
-            if (mouseIsDown) {
-
+        if (mouseIsDown) {
+            if (hoveringOnDocument) {
                 if (docX < 0) {
                     docX = 0;
                 }
-                
+                    
                 if (docX + splitter.offsetWidth > container.offsetWidth) {
                     docX = containerWidth - splitter.offsetWidth;
                 }
-
 
                 splitter.style.left = docX + 'px';
                 leftSide.style.width = splitter.offsetLeft + 'px';
                 rightSide.style.width = (container.offsetWidth - leftSide.offsetWidth - splitter.offsetWidth) + 'px';
                 rightSide.style.left = splitter.offsetLeft + splitter.offsetWidth + 'px';
-            }
-        } else {
-
-            let containerLeft = container.getBoundingClientRect().x;
-            if (mouseIsDown) {
-
+            } else {
                 if (globalXCoordinate + splitter.offsetWidth > containerWidth) {
                     globalXCoordinate = containerWidth - splitter.offsetWidth;
                 }
@@ -117,43 +105,5 @@ const SplitterBar = function(container) {
                 rightSide.style.left = splitter.offsetLeft + splitter.offsetWidth + 'px';
             }
         }
-
-        
-    });
-
-    document.addEventListener('mousemove', function(evt) {
-        // evt.preventDefault();
-        
-        // if (mouseIsDown) {
-
-        //     //let left = 8;
-        //     //globalXCoordinate = left + evt.offsetX - startX;
-
-        //     //if (splitter.offsetLeft + splitter.offsetWidth > container.offsetWidth) {
-        //     //     globalXCoordinate = container.offsetWidth - splitter.offsetWidth;
-        //     //}
-        //     console.log(splitter.offsetLeft)
-        //     console.log(container.offsetLeft)
-
-        //     let left = container.offsetLeft;
-        //     globalXCoordinate = (evt.offsetX - startX - left);
-        //     //console.log('global', left)
-
-
-        //     if (splitter.offsetLeft < container.offsetLeft) {
-        //          globalXCoordinate = 0;
-        //     }
-
-        //     // if (splitter.offsetLeft + splitter.offsetWidth > container.offsetLeft + container.offsetWidth) {
-        //     //     globalXCoordinate = evt.offsetX;
-        //     // }
-
-
-        //     splitter.style.left = globalXCoordinate + 'px';
-        //     //splitter.style.left = evt.offsetX - startX - container.offsetLeft + 'px';
-        //     leftSide.style.width = splitter.offsetLeft + 'px';
-        //     rightSide.style.width = (container.offsetWidth - leftSide.offsetWidth - splitter.offsetWidth) + 'px';
-        //     rightSide.style.left = splitter.offsetLeft + splitter.offsetWidth + 'px';
-        // }
     });
 };
